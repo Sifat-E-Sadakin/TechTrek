fa1ba711aa75fed6fdaee1f90156fc622d120f05
Sifat-E-Sadakin <sifatesadakin10@gmail.comm>
2023-04-09 23:13:49 +0600

    route implemented + navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <div>
+            <div className="navbar bg-base-100 flex justify-between">
+                <div>
+                    <a className="btn btn-ghost normal-case text-xl">Hi Hackers</a>
+                </div>
+                
+                <div className=''>
+                    <Link className='m-2' to='/statistics'>Statistics</Link>
+                    <Link className='m-2'to='/appliedJobs'>Applied Jobs</Link>
+                    <Link className='m-2'to='/qna'>QNA</Link>
+                </div>
+                <div>
+                <button className='btn '>Start Applying</button>
+
+                </div>
+            
+            </div>
+            
+        </div>
+    );
+};
+
+export default Navbar;
