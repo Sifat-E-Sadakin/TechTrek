bc5c56490d0d558ce869e0566b8a6f8482117b24
Sifat-E-Sadakin <sifatesadakin10@gmail.comm>
2023-04-10 18:08:42 +0600

    show all btn implemented

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -4,6 +4,7 @@ import JobCategory from '../JobCategory/JobCategory';
 import { useLoaderData } from 'react-router-dom';
 import TrendingJobs from '../TrendingJobs/TrendingJobs';
 import { LoadTrendingJobs } from '../LoadData/LoadData';
+import { data } from 'autoprefixer';
 
 const Landing = () => {
 
@@ -11,15 +12,46 @@ const Landing = () => {
 
    let [trendingJobs, setTrendingJobs]= useState([]);
 
+  let [allJobList, setAllJobList]= useState([]);
+
+ 
+
    useEffect(()=>{
     
         fetch('TrendingJobs.json')
         .then(res=> res.json())
-        .then(data=> setTrendingJobs(data))
+        .then(data=> processData(data))
+
+        function processData(data){
+
+            setAllJobList(data);
+            
+            
+            let processedData=  data.slice(0,4);
+
+            setTrendingJobs(processedData);
+        }
+
+        
 },[]);
 
+function showAll(){
+    console.log(allJobList);
+    setTrendingJobs(allJobList);
+
+    let btn= document.getElementById('show-all');
+     btn.style.display= 'none';
+
+
+}
+
+
+
+
+
+
    
-//    console.log(trendingJobs);
+ 
 
    
     return (
@@ -29,6 +61,13 @@ const Landing = () => {
             <JobCategory categoryList={categoryList}></JobCategory>
 
             <TrendingJobs trendingJobs={trendingJobs}></TrendingJobs>
+
+            <div className='flex justify-center'>
+                 <button className='btn' onClick={showAll} id='show-all'>Show All Jobs</button>
+
+            </div>
+
+            
             
             
         </div>
